241d2324a06a97dfdb9e90c041cba53bf0e5eb43
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-11-26 14:06:22 -0500

    more config work to get the app up and running

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '@screens/HomeScreen';
-import RedirectScreen from '@screens/RedirectScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import RedirectScreen from '../screens/RedirectScreen';
 
-export type RootStackParamList = {
-  Home: undefined;
-  Redirect: { params: Record<string, string> } | undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Home' }} 
-        />
-        <Stack.Screen 
-          name="Redirect" 
-          component={RedirectScreen} 
-          options={{ title: 'Redirect Viewer' }} 
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Redirect" component={RedirectScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
