b30b1a9ad131a84c3e8409970dd9685ad5dfdf49
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-12-07 19:33:50 -0500

    making some good progress to getting it running

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { registerRootComponent } from 'expo';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// This ensures the app works correctly in the Expo and web environments.
+registerRootComponent(App);
